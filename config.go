7a84f61754ca1e123fcb2a325ec06c66e13afee7
Andrey Semochkin <admin@deepweb.spb.ru>
2017-04-23 04:55:00 +0300

    Update config.go

--- config.go
+++ config.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 //DecodeFile Decode Config File
@@ -26,12 +27,24 @@ func DecodeFile(fpath string, v interface{}) error {
 	}
 	return nil
 }
+
+//EncodeFile Encode Config File
+func EncodeFile(fpath string, v interface{}) error {
+	indent, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return fmt.Errorf("Marshal Config %v Error %v", fpath, err)
+	}
+	err = ioutil.WriteFile(fpath, formatArray(indent), 0644)
+	if err != nil {
+		return fmt.Errorf("Write Config %v Error %v", fpath, err)
+	}
+	return nil
+}
 func lineAndCharacter(input string, offset int) (line int, character int, err error) {
 	lf := rune(0x0A)
 	if offset > len(input) || offset < 0 {
-		return 0, 0, fmt.Errorf("Couldn't find offset %d within the input.", offset)
+		return 0, 0, fmt.Errorf("Couldn't find offset %d within the input", offset)
 	}
-	// Humans tend to count from 1.
 	line = 1
 	for i, b := range input {
 		if b == lf {
@@ -44,4 +57,30 @@ func lineAndCharacter(input string, offset int) (line int, character int, err er
 		}
 	}
 	return line, character, nil
+}
+func formatArray(indent []byte) []byte {
+	var result string
+	var pre string
+	var start bool
+	for _, val := range strings.Split(string(indent), "\n") {
+		if strings.Contains(val, "[") {
+			start = true
+		}
+		if strings.Contains(val, "]") {
+			start = false
+		}
+		if strings.Contains(val, "[") && !strings.Contains(pre, "[") {
+			result += strings.Trim(val, "\n")
+		} else if strings.Contains(val, "[") && strings.Contains(pre, "[") {
+			result += "\n" + val
+		} else if strings.Contains(val, "]") && !strings.Contains(pre, "]") {
+			result += strings.Trim(strings.Trim(strings.Trim(val, "\n"), " "), "	") + "\n"
+		} else if start {
+			result += strings.Trim(strings.Trim(strings.Trim(val, "\n"), " "), "	")
+		} else if !start {
+			result += val + "\n"
+		}
+		pre = val
+	}
+	return []byte(result)
 }
